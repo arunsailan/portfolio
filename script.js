3e12ca63112d47df3897bdfc5a0033a79907c63b
Arun Sailan <54799585+arunsailan@users.noreply.github.com>
2026-07-08 09:44:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,28 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
-
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 150;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
